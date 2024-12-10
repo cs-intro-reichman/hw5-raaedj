7121edcb889090a1ff972c8683c1f35db1dabde5
raaedj <raed.agbaria@post.runi.ac.il>
2024-12-10 20:31:24 +0200

    Raed

--- Scrabble.java
+++ Scrabble.java
@@ -116,13 +116,13 @@ public class Scrabble {
 			} else {
 				int score = wordScore(word);
 				totalScore += score;
-				System.out.println(word + " earned " + score + " points. Score: " + totalScore + " points");
+				System.out.println(word + " earned " + score + " points. Score: " + totalScore + " points\n");
 				hand = MyString.remove(hand, word);
 			}
 			System.out.println();
 		}
 	
-		System.out.println("End of hand. Total score: " + totalScore + " points\n");
+		System.out.println("End of hand. Total score: " + totalScore + " points");
 	}
 	
     public static void playGame() {
